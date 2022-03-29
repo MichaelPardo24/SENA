87c509e7a7c53f248231d68851da50935e3f085c
AndresBorda24 <ajborda4@misena.edu.co>
2022-03-29 17:47:04 -0500

    Correcciones minimas en composer.json, composer.lock & algunas vistas

--- resources/views/admin/fichas/edit.blade.php
+++ resources/views/admin/fichas/edit.blade.php
@@ -129,61 +129,79 @@
                         
                         <div class="p-3 shadow-lg bg-purple-200 rounded">
                             {{-- Formulario de eliminacion --}}
-                            <p class="italic text-purple-600 text-sm text-center">De de baja a una ficha</p>
-                            <form action="{{ route('fichas.soft-delete', $ficha) }}" method="post">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="block w-full items-center px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-500 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:ring focus:ring-purple-300 disabled:opacity-25 transition">
+                            <p class="italic text-purple-600 text-sm text-center">Dar de baja a una ficha</p>
+                               <div class="p-3">
+                                <button data-modal-toggle="popup-modal-archivar" data-id="{{ $ficha->id }}" class="block w-full items-center px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-500 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:ring focus:ring-purple-300 disabled:opacity-25 transition">
                                     Archivar
                                 </button>
-                            </form>
-                        </div>
 
-                        <div class="p-3 shadow-lg bg-red-300 rounded">
-                            {{-- Formulario de eliminacion --}}
-                            <p class="italic text-red-600 text-sm text-center">Cuidado, la ficha se eliminará completamente</p>
-                            <form action="{{ route('fichas.destroy', $ficha) }}" method="post">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="block w-full items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">
-                                    ELIMINAR
-                                </button>
-                            </form>
-                          
-                         {{-- Modal --}}
-                        <div class="mt-2 p-2">
-                            <x-jet-danger-button class="px-1 flex-auto w-full" data-modal-toggle="popup-modal" data-id="{{ $ficha->id }}" >ELIMINAR</x-jet-danger-button>
-                                <!-- Delete Product Modal -->
-                            <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="popup-modal">
-                                <div class="relative px-4 w-full max-w-md h-full md:h-auto">
-                                    <!-- Modal content -->
-                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <!-- Modal header -->
-                                        <div class="flex justify-end p-2">
-                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
-                                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                            </button>
-                                        </div>
-                                        <!-- Modal body -->
-                                        <div class="p-6 pt-0 text-center">
-                                            <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-red-400">¿Esta seguro que quiere eliminar esta ficha?</h3>
-                                            <form action="{{ route('fichas.destroy', $ficha->id) }}" method="POST" class="m-3">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button data-modal-toggle="popup-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                                    Si, estoy seguro
+                                <!-- Soft Delete Modal -->
+                                <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="popup-modal-archivar">
+                                    <div class="relative px-4 w-full max-w-md h-full md:h-auto">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <div class="flex justify-end p-2">
+                                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal-archivar">
+                                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                                 </button>
-                                            </form>
-                                            <button data-modal-toggle="popup-modal" type="button" class="text-red-500 bg-white hover:bg-red-100 focus:ring-4 focus:ring-red-300 rounded-lg border border-red-200 text-sm font-medium px-5 py-2.5 hover:text-red-900 focus:z-10 dark:bg-red-700 dark:text-red-300 dark:border-red-500 dark:hover:text-white dark:hover:bg-red-600">No, cancelar</button>
+                                            </div>
+                                            <!-- Modal body -->
+                                            <div class="p-6 pt-0 text-center">
+                                                <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-red-400">¿Esta seguro que quiere dar de baja esta ficha?</h3>
+                                                <form action="{{ route('fichas.soft-delete', $ficha) }}" method="post">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="items-center px-4 py-2 my-2.5 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-purple-500 active:bg-purple-900 focus:outline-none focus:border-purple-900 focus:ring focus:ring-purple-300 disabled:opacity-25 transition">
+                                                        Si, estoy seguro
+                                                    </button>
+                                                </form>
+                                                <button data-modal-toggle="popup-modal-archivar" type="button" class="text-red-500 bg-white hover:bg-red-100 focus:ring-4 focus:ring-red-300 rounded-lg border border-red-200 text-sm font-medium px-5 py-2.5 hover:text-red-900 focus:z-10 dark:bg-red-700 dark:text-red-300 dark:border-red-500 dark:hover:text-white dark:hover:bg-red-600">No, cancelar</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="p-3 shadow-lg bg-red-300 rounded">
+                            {{-- Formulario de eliminacion --}}
+                            <p class="italic text-red-600 text-sm text-center">Cuidado, la ficha se eliminará completamente</p>
+                          
+                         {{-- Modal --}}
+                            <div class="p-3">
+                                <x-jet-danger-button class="px-1 flex-auto w-full" data-modal-toggle="popup-modal" data-id="{{ $ficha->id }}" >ELIMINAR</x-jet-danger-button>
+                                    <!-- Delete Product Modal -->
+                                <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="popup-modal">
+                                    <div class="relative px-4 w-full max-w-md h-full md:h-auto">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <div class="flex justify-end p-2">
+                                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                </button>
+                                            </div>
+                                            <!-- Modal body -->
+                                            <div class="p-6 pt-0 text-center">
+                                                <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-red-400">¿Esta seguro que quiere eliminar esta ficha?</h3>
+                                                <form action="{{ route('fichas.destroy', $ficha->id) }}" method="POST" class="m-3">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button data-modal-toggle="popup-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                        Si, estoy seguro
+                                                    </button>
+                                                </form>
+                                                <button data-modal-toggle="popup-modal" type="button" class="text-red-500 bg-white hover:bg-red-100 focus:ring-4 focus:ring-red-300 rounded-lg border border-red-200 text-sm font-medium px-5 py-2.5 hover:text-red-900 focus:z-10 dark:bg-red-700 dark:text-red-300 dark:border-red-500 dark:hover:text-white dark:hover:bg-red-600">No, cancelar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
