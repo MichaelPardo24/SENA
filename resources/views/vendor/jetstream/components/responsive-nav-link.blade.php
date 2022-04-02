9cc692fd87fd6ff803fdefd249c078efd2b98854
MichaelPardo24 <maicol.pardo24@hotmail.com>
2022-04-01 20:12:40 -0500

    Diseño nav

--- resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
+++ resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block pl-3 pr-4 py-2 border-l-4 border-orange-400 text-base font-medium text-orange-700 bg-orange-50 focus:outline-none focus:text-orange-800 focus:bg-orange-100 focus:border-orange-700 transition'
-            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition';
+            ? 'block pl-3 pr-4 py-2 border-l-4 border-orange-500 text-base font-medium text-orange-500 bg-orange-50 focus:outline-none focus:text-orange-600 focus:bg-orange-50 focus:border-orange-600 transition'
+            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-orange-300 hover:border-orange-300 focus:outline-none focus:text-orange-400 focus:border-orange-400 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
