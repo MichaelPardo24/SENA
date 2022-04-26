2f0674f21d4f508f89912dd492c6ddf4e27637be
MichaelPardo24 <maicol.pardo24@hotmail.com>
2022-04-26 18:48:11 -0500

    Url accesible solo para inst. tec. con ficha asignada

--- app/Http/Controllers/InsTecnico/ApprenticesController.php
+++ app/Http/Controllers/InsTecnico/ApprenticesController.php
@@ -10,8 +10,19 @@ class ApprenticesController extends Controller
 {
     public function index(Ficha $ficha)
     {
-        return view('ins-tecnico.apprentices.index', [
-            'ficha' => $ficha
-        ]);
+        $fichas = Ficha::withTrashed()->withCount('users')->whereHas('users.roles', function ($q) {
+            $q->where('roles.name', 'Instructor Tecnico');
+            $q->where('users.id', auth()->id());
+        })
+        ->get();
+
+        foreach ($fichas as $authenticatedUserFicha) {
+            if ($authenticatedUserFicha->code == $ficha->code) {
+                return view('ins-tecnico.apprentices.index', [
+                    'ficha' => $ficha
+                ]);
+            }
+        }
+        abort(404);
     }
 }
